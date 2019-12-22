9ac0bd1068abf1052e7720b09641fe96f124bba4
k4m4 <nikolaskam@gmail.com>
2019-12-22 13:44:58 -0500

    Remove redundant code

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const onion = opts => opts && opts.exact ?
 	new RegExp(`(?:^${v2}$)|(?:^${v3}$)`) :
 	new RegExp(`${v2}|${v3}`, 'g');
 
-onion.v2 = opts => opts && opts.exact ? new RegExp(`^${v2}$`) : new RegExp(`${b}${v2}${b}`, 'g');
-onion.v3 = opts => opts && opts.exact ? new RegExp(`^${v3}$`) : new RegExp(`${b}${v3}${b}`, 'g');
+onion.v2 = opts => opts && opts.exact ? new RegExp(`^${v2}$`) : new RegExp(v2, 'g');
+onion.v3 = opts => opts && opts.exact ? new RegExp(`^${v3}$`) : new RegExp(v3, 'g');
 
 module.exports = onion;
